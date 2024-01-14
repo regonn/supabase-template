2ce1d929d65e537efc3b1a7454179f03f703ac4f
regonn <programmer@haizine.art>
2024-01-14 16:51:28 +0900

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Supabase Template
+
+- copy `.env.example` to `.env.local`
+- fill supabase data to `.env.local`
---
+++ app/page.tsx
@@ -0,0 +1,51 @@
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
+export default async function Index() {
+  const cookieStore = cookies();
+
+  const canInitSupabaseClient = () => {
+    try {
+      createClient(cookieStore);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  };
+
+  const isSupabaseConnected = canInitSupabaseClient();
+  const supabase = createClient(cookieStore);
+  // notes(title: string)
+  //  -- SQL
+  //  create table notes (
+  //   id serial primary key,
+  //   title text
+  // );
+  const { data: notes } = await supabase.from("notes").select();
+
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
+        <main className="flex-1 flex flex-col gap-6">
+          {isSupabaseConnected && notes!.map((note) => (<p>
+            {note.title}
+          </p>))}
+        </main>
+      </div>
+
+      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+        <p>
+          Powered by{" "}
+          <a
+            href="https://supabase.com/"
+            target="_blank"
+            className="font-bold hover:underline"
+            rel="noreferrer"
+          >
+            Supabase
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+}
